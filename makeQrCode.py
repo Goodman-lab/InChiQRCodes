2c036dc13f9470865ca28243904893d652412038
EdmundGoodman <egoodman3141@gmail.com>
2021-01-09 23:49:28 +0000

    Development of source continued on University machines, which could not be tracked, as git was not installed. Final source code as of 09/01/2021 committed from copy taken off live server

--- makeQrCode.py
+++ makeQrCode.py
@@ -1,27 +1,43 @@
 #!/usr/bin/env python3
-import qrcode
 
+import sys
+sys.path.append('/home/jmg11/.local/lib/python3.5/site-packages')
+import qrcode
 from pathlib import Path
 
 def makeQrCode(InChiCode):
     #Get the path to write the image to, based of the content of the qr code,
     #to avoid duplication problems
-    prefix = "qr/"
+    prefix = "./qr/"
     qrPath = "{}{}.png".format(prefix, InChiCode)
+    txtPath = "{}{}.txt".format(prefix, InChiCode)
 
     if Path(qrPath).exists():
         #If it exists, update the date, but don't remake it
         Path(qrPath).touch()
+        os.remove(txtPath)
     else:
         #If it doesn't exist, generate and save it
-        img = qrcode.make(InChiCode)
+        img = qrcode.make("https://pubchem.ncbi.nlm.nih.gov/#query="+InChiCode)
         img.save(qrPath, "PNG")
+        os.remove(txtPath)
+
+
+
+
+
 
 if __name__ == "__main__":
     from sys import argv
 
     if len(argv) >= 2:
         #If there are enough arguments, make the qr code, and exit with cleanly
+        #print("creating qrinchi")
+        #print('\n'.join(sys.path))
+        #print('\n')
+        #with open("qr/"+argv[1]+".ttt","w+") as f:
+        #    f.write(argv[1]);
+
         makeQrCode(argv[1])
         exit(0)
     else:
